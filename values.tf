b34112d566216dd10517af5f7a0af71dc61ad652
Stefan Roman <stefanroman129@gmail.com>
2024-01-16 15:31:00 +0100

    feat: support for Kyverno chart 3.1.3

--- values.tf
+++ values.tf
@@ -1,12 +1,47 @@
 locals {
   values_default = yamlencode({
     # add default values here
-    "rbac" : {
-      "serviceAccount" : {
-        "create" : var.service_account_create
-        "name" : var.service_account_name
-        "annotations" : {
-          "eks.amazonaws.com/role-arn" : local.irsa_role_create ? aws_iam_role.this[0].arn : ""
+    "admissionController" : {
+      "rbac" : {
+        "serviceAccount" : {
+          "create" : var.service_account_create
+          "name" : "${var.service_account_name}-admission-controller"
+          "annotations" : {
+            "eks.amazonaws.com/role-arn" : local.irsa_role_create ? aws_iam_role.this[0].arn : ""
+          }
+        }
+      }
+    }
+    "backgroundController" : {
+      "rbac" : {
+        "serviceAccount" : {
+          "create" : var.service_account_create
+          "name" : "${var.service_account_name}-background-controller"
+          "annotations" : {
+            "eks.amazonaws.com/role-arn" : local.irsa_role_create ? aws_iam_role.this[0].arn : ""
+          }
+        }
+      }
+    }
+    "cleanupController" : {
+      "rbac" : {
+        "serviceAccount" : {
+          "create" : var.service_account_create
+          "name" : "${var.service_account_name}-cleanup-controller"
+          "annotations" : {
+            "eks.amazonaws.com/role-arn" : local.irsa_role_create ? aws_iam_role.this[0].arn : ""
+          }
+        }
+      }
+    }
+    "reportsController" : {
+      "rbac" : {
+        "serviceAccount" : {
+          "create" : var.service_account_create
+          "name" : "${var.service_account_name}-reports-controller"
+          "annotations" : {
+            "eks.amazonaws.com/role-arn" : local.irsa_role_create ? aws_iam_role.this[0].arn : ""
+          }
         }
       }
     }
