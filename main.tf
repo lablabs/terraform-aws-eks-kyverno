1d9fbb5bea36b599a0c4029fbe5595c95e2ab478
Attila Sovak <attila.sovak@gmail.com>
2025-05-27 14:54:55 +0200

    feat!: add fixed components names and use common format (#7)
    * fix(kyverno): add fixed components names and use common format
    
    * fix(kyverno): add rbac create option per component
    
    * fix(kyverno): remove prefix

--- main.tf
+++ main.tf
@@ -16,81 +16,105 @@ locals {
   }
 
   addon_irsa = {
-    "${local.addon.name}-admission-controller" = {
-      rbac_create              = var.admission_controller_rbac_create
-      service_account_create   = var.admission_controller_service_account_create
-      service_account_name     = var.admission_controller_service_account_name
-      irsa_role_create         = var.admission_controller_irsa_role_create
-      irsa_role_name           = "admission"
-      irsa_additional_policies = var.admission_controller_irsa_additional_policies
+    "admission-controller" = {
+      rbac_create            = var.admission_controller_rbac_create
+      service_account_create = var.admission_controller_service_account_create
+      service_account_name   = var.admission_controller_service_account_name
+
+      irsa_role_create          = var.admission_controller_irsa_role_create
+      irsa_policy_enabled       = var.admission_controller_irsa_policy_enabled
+      irsa_policy               = var.admission_controller_irsa_policy
+      irsa_assume_role_enabled  = var.admission_controller_irsa_assume_role_enabled
+      irsa_assume_role_arns     = var.admission_controller_irsa_assume_role_arns
+      irsa_permissions_boundary = var.admission_controller_irsa_permissions_boundary
+      irsa_additional_policies  = var.admission_controller_irsa_additional_policies
     }
-    "${local.addon.name}-background-controller" = {
-      rbac_create              = var.background_controller_rbac_create
-      service_account_name     = var.background_controller_service_account_create
-      service_account_name     = var.background_controller_service_account_name
-      irsa_role_create         = var.background_controller_irsa_role_create
-      irsa_role_name           = "background"
-      irsa_additional_policies = var.background_controller_irsa_additional_policies
+    "background-controller" = {
+      rbac_create            = var.background_controller_rbac_create
+      service_account_create = var.background_controller_service_account_create
+      service_account_name   = var.background_controller_service_account_name
+
+      irsa_role_create          = var.background_controller_irsa_role_create
+      irsa_policy_enabled       = var.background_controller_irsa_policy_enabled
+      irsa_policy               = var.background_controller_irsa_policy
+      irsa_assume_role_enabled  = var.background_controller_irsa_assume_role_enabled
+      irsa_assume_role_arns     = var.background_controller_irsa_assume_role_arns
+      irsa_permissions_boundary = var.background_controller_irsa_permissions_boundary
+      irsa_additional_policies  = var.background_controller_irsa_additional_policies
     }
-    "${local.addon.name}-cleanup-controller" = {
-      rbac_create              = var.cleanup_controller_rbac_create
-      service_account_name     = var.cleanup_controller_service_account_create
-      service_account_name     = var.cleanup_controller_service_account_name
-      irsa_role_create         = var.cleanup_controller_irsa_role_create
-      irsa_role_name           = "cleanup"
-      irsa_additional_policies = var.cleanup_controller_irsa_additional_policies
+    "cleanup-controller" = {
+      rbac_create            = var.cleanup_controller_rbac_create
+      service_account_create = var.cleanup_controller_service_account_create
+      service_account_name   = var.cleanup_controller_service_account_name
+
+      irsa_role_create          = var.cleanup_controller_irsa_role_create
+      irsa_policy_enabled       = var.cleanup_controller_irsa_policy_enabled
+      irsa_policy               = var.cleanup_controller_irsa_policy
+      irsa_assume_role_enabled  = var.cleanup_controller_irsa_assume_role_enabled
+      irsa_assume_role_arns     = var.cleanup_controller_irsa_assume_role_arns
+      irsa_permissions_boundary = var.cleanup_controller_irsa_permissions_boundary
+      irsa_additional_policies  = var.cleanup_controller_irsa_additional_policies
     }
-    "${local.addon.name}-reports-controller" = {
-      rbac_create              = var.reports_controller_rbac_create
-      service_account_name     = var.reports_controller_service_account_create
-      service_account_name     = var.reports_controller_service_account_name
-      irsa_role_create         = var.reports_controller_irsa_role_create
-      irsa_role_name           = "reports"
-      irsa_additional_policies = var.reports_controller_irsa_additional_policies
+    "reports-controller" = {
+      rbac_create            = var.reports_controller_rbac_create
+      service_account_create = var.reports_controller_service_account_create
+      service_account_name   = var.reports_controller_service_account_name
+
+      irsa_role_create          = var.reports_controller_irsa_role_create
+      irsa_policy_enabled       = var.reports_controller_irsa_policy_enabled
+      irsa_policy               = var.reports_controller_irsa_policy
+      irsa_assume_role_enabled  = var.reports_controller_irsa_assume_role_enabled
+      irsa_assume_role_arns     = var.reports_controller_irsa_assume_role_arns
+      irsa_permissions_boundary = var.reports_controller_irsa_permissions_boundary
+      irsa_additional_policies  = var.reports_controller_irsa_additional_policies
     }
   }
 
   addon_values = yamlencode({
     admissionController = {
+      enabled = var.admission_controller_enabled
       rbac = {
-        create = module.addon-irsa["${local.addon.name}-admission-controller"].rbac_create
+        create = module.addon-irsa["admission-controller"].rbac_create
         serviceAccount = {
-          name = module.addon-irsa["${local.addon.name}-admission-controller"].service_account_name
-          annotations = module.addon-irsa["${local.addon.name}-admission-controller"].irsa_role_enabled ? {
-            "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-admission-controller"].iam_role_attributes.arn
+          name = module.addon-irsa["admission-controller"].service_account_name
+          annotations = module.addon-irsa["admission-controller"].irsa_role_enabled ? {
+            "eks.amazonaws.com/role-arn" = module.addon-irsa["admission-controller"].iam_role_attributes.arn
           } : tomap({})
         }
       }
     }
     backgroundController = {
+      enabled = var.background_controller_enabled
       rbac = {
-        create = module.addon-irsa["${local.addon.name}-background-controller"].rbac_create
+        create = module.addon-irsa["background-controller"].rbac_create
         serviceAccount = {
-          name = module.addon-irsa["${local.addon.name}-background-controller"].service_account_name
-          annotations = module.addon-irsa["${local.addon.name}-background-controller"].irsa_role_enabled ? {
-            "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-background-controller"].iam_role_attributes.arn
+          name = module.addon-irsa["background-controller"].service_account_name
+          annotations = module.addon-irsa["background-controller"].irsa_role_enabled ? {
+            "eks.amazonaws.com/role-arn" = module.addon-irsa["background-controller"].iam_role_attributes.arn
           } : tomap({})
         }
       }
     }
     cleanupController = {
+      enabled = var.cleanup_controller_enabled
       rbac = {
-        create = module.addon-irsa["${local.addon.name}-cleanup-controller"].rbac_create
+        create = module.addon-irsa["cleanup-controller"].rbac_create
         serviceAccount = {
-          name = module.addon-irsa["${local.addon.name}-cleanup-controller"].service_account_name
-          annotations = module.addon-irsa["${local.addon.name}-cleanup-controller"].irsa_role_enabled ? {
-            "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-cleanup-controller"].iam_role_attributes.arn
+          name = module.addon-irsa["cleanup-controller"].service_account_name
+          annotations = module.addon-irsa["cleanup-controller"].irsa_role_enabled ? {
+            "eks.amazonaws.com/role-arn" = module.addon-irsa["cleanup-controller"].iam_role_attributes.arn
           } : tomap({})
         }
       }
     }
     reportsController = {
+      enabled = var.reports_controller_enabled
       rbac = {
-        create = module.addon-irsa["${local.addon.name}-reports-controller"].rbac_create
+        create = module.addon-irsa["reports-controller"].rbac_create
         serviceAccount = {
-          name = module.addon-irsa["${local.addon.name}-reports-controller"].service_account_name
-          annotations = module.addon-irsa["${local.addon.name}-reports-controller"].irsa_role_enabled ? {
-            "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-reports-controller"].iam_role_attributes.arn
+          name = module.addon-irsa["reports-controller"].service_account_name
+          annotations = module.addon-irsa["reports-controller"].irsa_role_enabled ? {
+            "eks.amazonaws.com/role-arn" = module.addon-irsa["reports-controller"].iam_role_attributes.arn
           } : tomap({})
         }
       }
--- variables.tf
+++ variables.tf
@@ -11,28 +11,64 @@ variable "admission_controller_rbac_create" {
   description = "Whether to create and use Admission Controller RBAC resources."
 }
 
+variable "admission_controller_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to true to enable Thanos Query component"
+}
+
 variable "admission_controller_service_account_create" {
   type        = bool
   default     = true
-  description = "Whether to create the Service Account for the Admission Controller."
+  description = "Whether to create the Service Account for the Kyverno Admission Controller component."
 }
 
 variable "admission_controller_service_account_name" {
   type        = string
-  default     = "admission-controler"
-  description = "The name of the Service Account for the Admission Controller."
+  default     = "admission-controller"
+  description = "The name of the Service Account for the Kyverno Admission Controller component."
 }
 
 variable "admission_controller_irsa_role_create" {
   type        = bool
-  default     = true
-  description = "Whether to create the IRSA role for the Admission Controller."
+  default     = false
+  description = "Whether to create the IRSA role for the Kyverno Admission Controller component."
+}
+
+variable "admission_controller_irsa_policy_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether to create IAM policy specified by `admission_controller_irsa_policy` for the Kyverno Admission Controller component. Mutually exclusive with `admission_controller_irsa_assume_role_enabled`."
+}
+
+variable "admission_controller_irsa_policy" {
+  type        = string
+  default     = ""
+  description = "Policy to be attached to the default role of the Kyverno Admission Controller component. Applied only if `admission_controller_irsa_policy_enabled` is `true`."
+}
+
+variable "admission_controller_irsa_assume_role_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether IRSA for the Kyverno Admission Controller component is allowed to assume role defined by `admission_controller_irsa_assume_role_arn`. Mutually exclusive with `admission_controller_irsa_policy_enabled`."
+}
+
+variable "admission_controller_irsa_assume_role_arns" {
+  type        = list(string)
+  default     = []
+  description = "Assume role ARNs for the Kyverno Admission Controller component. Applied only if `admission_controller_irsa_assume_role_enabled` is `true`."
+}
+
+variable "admission_controller_irsa_permissions_boundary" {
+  type        = string
+  default     = null
+  description = "ARN of the policy that is used to set the permissions boundary for the IRSA role of the Kyverno Admission Controller component. Defaults to `\"\"`."
 }
 
 variable "admission_controller_irsa_additional_policies" {
   type        = map(string)
   default     = {}
-  description = "Map of the additional policies to be attached to default role of the Admission Controller. Where key is arbitrary id and value is policy ARN."
+  description = "Map of the additional policies to be attached to default role of the Kyverno Admission Controller component. Where key is arbitrary id and value is policy ARN."
 }
 
 variable "background_controller_rbac_create" {
@@ -41,28 +77,64 @@ variable "background_controller_rbac_create" {
   description = "Whether to create and use Background Controller RBAC resources."
 }
 
+variable "background_controller_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to true to enable Thanos Query component"
+}
+
 variable "background_controller_service_account_create" {
   type        = bool
   default     = true
-  description = "Whether to create the Service Account for the Background Operator."
+  description = "Whether to create the Service Account for the Kyverno Background Controller component."
 }
 
 variable "background_controller_service_account_name" {
   type        = string
-  default     = "background-controler"
-  description = "The name of the Service Account for the Background Operator."
+  default     = "background-controller"
+  description = "The name of the Service Account for the Kyverno Background Controller component."
 }
 
 variable "background_controller_irsa_role_create" {
   type        = bool
-  default     = true
-  description = "Whether to create the IRSA role for the Background Operator."
+  default     = false
+  description = "Whether to create the IRSA role for the Kyverno Background Controller component."
+}
+
+variable "background_controller_irsa_policy_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether to create IAM policy specified by `background_controller_irsa_policy` for the Kyverno Background Controller component. Mutually exclusive with `background_controller_irsa_assume_role_enabled`."
+}
+
+variable "background_controller_irsa_policy" {
+  type        = string
+  default     = ""
+  description = "Policy to be attached to the default role of the Kyverno Background Controller component. Applied only if `background_controller_irsa_policy_enabled` is `true`."
+}
+
+variable "background_controller_irsa_assume_role_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether IRSA for the Kyverno Background Controller component is allowed to assume role defined by `background_controller_irsa_assume_role_arn`. Mutually exclusive with `background_controller_irsa_policy_enabled`."
+}
+
+variable "background_controller_irsa_assume_role_arns" {
+  type        = list(string)
+  default     = []
+  description = "Assume role ARNs for the Kyverno Background Controller component. Applied only if `background_controller_irsa_assume_role_enabled` is `true`."
+}
+
+variable "background_controller_irsa_permissions_boundary" {
+  type        = string
+  default     = null
+  description = "ARN of the policy that is used to set the permissions boundary for the IRSA role of the Kyverno Background Controller component. Defaults to `\"\"`."
 }
 
 variable "background_controller_irsa_additional_policies" {
   type        = map(string)
   default     = {}
-  description = "Map of the additional policies to be attached to default role of the Background Operator. Where key is arbitrary id and value is policy ARN."
+  description = "Map of the additional policies to be attached to default role of the Kyverno Background Controller component. Where key is arbitrary id and value is policy ARN."
 }
 
 variable "cleanup_controller_rbac_create" {
@@ -71,29 +143,64 @@ variable "cleanup_controller_rbac_create" {
   description = "Whether to create and use Cleanup Controller RBAC resources."
 }
 
+variable "cleanup_controller_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to true to enable Thanos Query component"
+}
+
 variable "cleanup_controller_service_account_create" {
   type        = bool
   default     = true
-  description = "Set to false to prevent the Cleanup Controller Service Account from being created."
+  description = "Whether to create the Service Account for the Kyverno Cleanup Controller component."
 }
 
 variable "cleanup_controller_service_account_name" {
   type        = string
-  default     = "cleanup_controller"
-  description = "Name of the Cleanup Controller Service Account."
-  nullable    = false
+  default     = "cleanup-controller"
+  description = "The name of the Service Account for the Kyverno Cleanup Controller component."
 }
 
 variable "cleanup_controller_irsa_role_create" {
   type        = bool
-  default     = true
-  description = "Set to false to prevent the Cleanup Controller IRSA role from being created."
+  default     = false
+  description = "Whether to create the IRSA role for the Kyverno Cleanup Controller component."
+}
+
+variable "cleanup_controller_irsa_policy_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether to create IAM policy specified by `cleanup_controller_irsa_policy` for the Kyverno Cleanup Controller component. Mutually exclusive with `cleanup_controller_irsa_assume_role_enabled`."
+}
+
+variable "cleanup_controller_irsa_policy" {
+  type        = string
+  default     = ""
+  description = "Policy to be attached to the default role of the Kyverno Cleanup Controller component. Applied only if `cleanup_controller_irsa_policy_enabled` is `true`."
+}
+
+variable "cleanup_controller_irsa_assume_role_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether IRSA for the Kyverno Cleanup Controller component is allowed to assume role defined by `cleanup_controller_irsa_assume_role_arn`. Mutually exclusive with `cleanup_controller_irsa_policy_enabled`."
+}
+
+variable "cleanup_controller_irsa_assume_role_arns" {
+  type        = list(string)
+  default     = []
+  description = "Assume role ARNs for the Kyverno Cleanup Controller component. Applied only if `cleanup_controller_irsa_assume_role_enabled` is `true`."
+}
+
+variable "cleanup_controller_irsa_permissions_boundary" {
+  type        = string
+  default     = null
+  description = "ARN of the policy that is used to set the permissions boundary for the IRSA role of the Kyverno Cleanup Controller component. Defaults to `\"\"`."
 }
 
 variable "cleanup_controller_irsa_additional_policies" {
   type        = map(string)
   default     = {}
-  description = "Additional IAM policies to attach to the Cleanup Controller IRSA role."
+  description = "Map of the additional policies to be attached to default role of the Kyverno Cleanup Controller component. Where key is arbitrary id and value is policy ARN."
 }
 
 variable "reports_controller_rbac_create" {
@@ -102,27 +209,62 @@ variable "reports_controller_rbac_create" {
   description = "Whether to create and use Reports Controller RBAC resources."
 }
 
+variable "reports_controller_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to true to enable Thanos Query component"
+}
+
 variable "reports_controller_service_account_create" {
   type        = bool
   default     = true
-  description = "Set to false to prevent the Reports Controller Service Account from being created."
+  description = "Whether to create the Service Account for the Kyverno Reports Controller component."
 }
 
 variable "reports_controller_service_account_name" {
   type        = string
-  default     = "reports_controller"
-  description = "Name of the Reports Controller Service Account."
-  nullable    = false
+  default     = "reports-controller"
+  description = "The name of the Service Account for the Kyverno Reports Controller component."
 }
 
 variable "reports_controller_irsa_role_create" {
   type        = bool
-  default     = true
-  description = "Set to false to prevent the Reports Controller IRSA role from being created."
+  default     = false
+  description = "Whether to create the IRSA role for the Kyverno Reports Controller component."
+}
+
+variable "reports_controller_irsa_policy_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether to create IAM policy specified by `reports_controller_irsa_policy` for the Kyverno Reports Controller component. Mutually exclusive with `reports_controller_irsa_assume_role_enabled`."
+}
+
+variable "reports_controller_irsa_policy" {
+  type        = string
+  default     = ""
+  description = "Policy to be attached to the default role of the Kyverno Reports Controller component. Applied only if `reports_controller_irsa_policy_enabled` is `true`."
+}
+
+variable "reports_controller_irsa_assume_role_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether IRSA for the Kyverno Reports Controller component is allowed to assume role defined by `reports_controller_irsa_assume_role_arn`. Mutually exclusive with `reports_controller_irsa_policy_enabled`."
+}
+
+variable "reports_controller_irsa_assume_role_arns" {
+  type        = list(string)
+  default     = []
+  description = "Assume role ARNs for the Kyverno Reports Controller component. Applied only if `reports_controller_irsa_assume_role_enabled` is `true`."
+}
+
+variable "reports_controller_irsa_permissions_boundary" {
+  type        = string
+  default     = null
+  description = "ARN of the policy that is used to set the permissions boundary for the IRSA role of the Kyverno Reports Controller component. Defaults to `\"\"`."
 }
 
 variable "reports_controller_irsa_additional_policies" {
   type        = map(string)
   default     = {}
-  description = "Additional IAM policies to attach to the Reports Controller IRSA role."
+  description = "Map of the additional policies to be attached to default role of the Kyverno Reports Controller component. Where key is arbitrary id and value is policy ARN."
 }
